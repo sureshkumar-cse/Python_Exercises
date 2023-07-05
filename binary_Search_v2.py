bfc13279aa9bc8f50729959a9a336013cf7e131e
Suresh Kumar <suresh@crm-masters.com>
2023-07-05 15:55:37 +0530

    Create version 2 of debugger with version 2 of binary search

---
+++ binary_Search_v2.py
@@ -0,0 +1,23 @@
+# Problem Statement: "Given a sorted array of integers, and an integer x, find the first index of the array containing the number x, or return -1 in case the number doesn't appear in the array".
+
+# Solution:
+
+# Binary Search on a sorted Array
+def solution(a, x):
+    l = 0
+    r = len(a) - 1
+    pos = -1
+    while l <= r:
+        m = (l + r) // 2
+        if a[m] >= x:
+            pos = m
+            r = m - 1
+        else:
+            l = m + 1
+
+    return pos
+    
+# a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+# x = 69   
+# output = solution(a, x)
+# print (output)
--- debugger_v2.py
+++ debugger_v2.py
@@ -2,14 +2,14 @@
 
 import random_Generator
 import linear_Search_v1
-import binary_Search_v1
+import binary_Search_v2
     
 def debug():
     test_cases = 10000
     for _ in range(test_cases):
         a, x = random_Generator.generate_input()
         no_brainer_output = linear_Search_v1.no_brainer(a, x)
-        solution_output = binary_Search_v1.solution(a, x)
+        solution_output = binary_Search_v2.solution(a, x)
         if no_brainer_output != solution_output:
             print("Test Case:")
             print(a, x)
